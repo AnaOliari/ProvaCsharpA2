6c08acf96db3cc62f299dec3a77cc6d48b5ab77c
AnaOliari <carolina97627687@gmail.com>
2024-06-19 22:54:57 -0300

    Continuação
    Criado a classe do banco e funcionario e no program o cadastro e a listagem de funcionarios

---
+++ Ana/Ana/Models/AppDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ana.Models;
+
+public class AppDataContext : DbContext
+{
+  //Quais classes vão representar as tabelas no banco
+  public DbSet<Funcionario> Funcionario { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=ana_ana.db");
+    }
+
+}
+
--- Ana/Ana/Program.cs
+++ Ana/Ana/Program.cs
@@ -1,6 +1,20 @@
+using Ana.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/api/funcionario/cadastrar", ([FromBody] Funcionario funcionario, [FromServices] AppDataContext cxt) =>
+{
+    cxt.Funcionario.Add(funcionario);
+    cxt.SaveChanges();
+    return Results.Created($"/produto/{funcionario.Id}", funcionario);
+});
+
+app.MapGet("/api/funcionario/listar", ([FromServices] AppDataContext cxt) =>
+{
+  return Results.Ok(cxt.Funcionario.ToList());
+});
 
 app.Run();
